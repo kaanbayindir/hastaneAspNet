a429d201a1d2d49c9011b37f0ada53dd6976bb98
kaanbayindir <kaanbayindirr@gmail.com>
2020-06-05 21:40:42 +0300

    Initial Commit

---
+++ WebSitesiInsecure/Models/Hasta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSitesiInsecure.Models
+{
+    public class Hasta
+    {
+        public string TCNO { get; set; }
+        public string Isim { get; set; }
+
+        public string Soyisim { get; set; }
+
+        public Char Cinsiyet { get; set; }
+
+        public string Telefon { get; set; }
+
+        public string Adres { get; set; }
+
+        public DateTime DogumTarihi { get; set; }
+
+
+    }
+}
